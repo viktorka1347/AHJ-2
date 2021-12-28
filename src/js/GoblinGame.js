bd44f6b33ed42e414d7538e4ff2efd3102c195b1
John Doe <johndoe@example.com>
2021-12-28 14:30:57 +0300

    main
    Signed-off-by: John Doe <johndoe@example.com>

---
+++ src/js/GoblinGame.js
@@ -0,0 +1,32 @@
+export default class GoblinGame {
+    constructor(board, size) {
+        this.board = board;
+        this.size = size;
+        this.activeIndex = -1;
+    }
+
+    init() {
+        let html = "";
+        for (let i = 0; i < this.size ** 2; i++) {
+            html += '<div class="hole"></div>';
+        }
+        this.board.innerHTML = html;
+        this.holes = this.board.querySelectorAll(".hole");
+
+        this.goblin = document.createElement("img");
+        this.goblin.src = "img/goblin.png";
+        this.goblin.className = "goblin";
+
+        this.moveGoblin();
+        setInterval(() => this.moveGoblin(), 1000);
+    }
+
+    moveGoblin() {
+        const oldIndex = this.activeIndex;
+        do {
+            this.activeIndex = Math.trunc(Math.random() * this.size ** 2);
+        } while (this.activeIndex === oldIndex);
+
+        this.holes[this.activeIndex].appendChild(this.goblin);
+    }
+}
